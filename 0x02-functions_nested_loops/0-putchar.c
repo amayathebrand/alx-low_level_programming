ff891dd86f5786513083e7318654547c7249bdaa
amayathebrand <charityepelle1@gmail.com git config --global user.email charityepelle1@gmail.com>
2022-09-13 18:15:28 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Print the word "_putchar"
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+  int i;
+  char p[] = "_putchar";
+
+  for (i - 0; i < 8; i++)
+{
+  _putchar(c + i);
+}
+  _putchar(10);
+}
