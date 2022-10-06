c749e5dcd2f5bf91ed41215f86f70a8b2b514870
amayathebrand <charityepelle1@gmail.com git config --global user.email charityepelle1@gmail.com>
2022-10-06 08:22:18 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,41 +1,25 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _memset - copy char
- * @s: string
- * @b: input
- * @n: bytes
- * Return: string
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
-}
-
-/**
- * _calloc - allocates memory for an array using malloc
- * @nmemb: n elements
- * @size: bytes
- * Return: pointer
+ * _calloc - allocates memory for an array, using malloc
+ *
+ * @nmemb: members of array
+ * @size: size of each member
+ * Return: void pointer
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	unsigned int i;
+	char *arr;
 
-	if (nmeb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-
-	if (p == NULL)
-		return (NULL);
-	_memset(p, 0, (nmemb * size));
-
-	return (p);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (arr);
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
+	return (arr);
 }
+
