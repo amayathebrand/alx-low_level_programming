51e175646d1f525f60f3fc1b57e52ea8710ac60e
amayathebrand <charityepelle1@gmail.com git config --global user.email charityepelle1@gmail.com>
2022-10-20 12:37:33 -0700

    linked list project

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,21 +3,22 @@
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
- * Return: number of elements in the list.
+ *
+ * Return: the number of nodes, and [0] (nil) if str is NULL
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t n = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
+		n++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem)
+	return (n)
 }
